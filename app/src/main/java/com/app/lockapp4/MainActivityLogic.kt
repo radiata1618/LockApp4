fbe190bf65dcdbb142fa5140e268df1844d8c2e3
haruk <haruki.u@gmail.com>
2022-08-16 12:21:45 +0900

    時間計算まわりのロジックをかなり修正

--- app/src/main/java/com/app/lockapp4/MainActivityLogic.kt
+++ app/src/main/java/com/app/lockapp4/MainActivityLogic.kt
@@ -9,9 +9,7 @@ import android.util.Log
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.room.Room
-import com.app.lockapp4.framework.database.AppDatabase
-import com.app.lockapp4.framework.database.InstantLockDao
-import com.app.lockapp4.framework.database.LockTimeDao
+import com.app.lockapp4.framework.database.*
 import com.app.lockapp4.framework.utl.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -36,26 +34,74 @@ fun getInstantLockDao(context: Context): InstantLockDao {
     return getDatabase(context).instantLockDatabaseDao
 }
 
+fun getNextOrDuringLockTimeDao(context: Context): NextOrDuringLockTimeDao {
+    return getDatabase(context).nextOrDuringLockTimeDatabaseDao
+}
+
 fun restartApp(context: Context) {
 
     GlobalScope.launch(Dispatchers.IO) {
         val instantLockDataList = getInstantLockDao(context).getAll()
+        val nextOrDuringLockTimeList = getNextOrDuringLockTimeDao(context).getAll()
 
-        if (instantLockDataList.isEmpty()) {
+        val unLockTimeCal:Calendar?=judgeNowLockedReturnUnlockTime(instantLockDataList,nextOrDuringLockTimeList)
 
-            val nextTime = getLockTimeDao(context).getNextToTimeIfScheduledLocking()
-
-            if (nextTime != null) {
-                Timber.d("■■■■■■■■■■■■■■■■■■■ifScheduledWakeUpの中のwakeUpActivity(context)の前")
-                wakeUpActivity(context)
+        if(unLockTimeCal==null){
+            //ロック中でない
+            //スケジュールされた未来のロック時間があるかどうか
+            if(nextOrDuringLockTimeList.isEmpty()){
+                //ロック中でなく、スケジュールされたロックもなし
             }else{
-
-                setRestartPlan(context)
+                setRestartPlan(context,commonTranslateLongToCalendar(nextOrDuringLockTimeList[0].endTimeInLong))
 
             }
-
-        } else {
+        }else{
+            //ロック中である
             wakeUpActivity(context)
+        }
+
+    }
+}
+
+
+fun judgeNowLockedReturnUnlockTime(instantLockDataList: List<InstantLock>, nextOrDuringLockTimeList:List<NextOrDuringLockTime>):Calendar?{//lockされていない場合はNULL
+
+    if (instantLockDataList.isEmpty()) {
+        //インスタントロックされていない
+        if(nextOrDuringLockTimeList.isNotEmpty()){
+            if(commonTranslateLongToCalendar(nextOrDuringLockTimeList[0].startTimeInLong).before(Calendar.getInstance())){
+                //インスタントロックされていないかつスケジュールロックされている
+                return commonTranslateLongToCalendar(nextOrDuringLockTimeList[0].endTimeInLong)
+            }else{
+                //インスタントロックされていないかつスケジュールロックされていない
+                return null
+            }
+        }else{
+            //インスタントロックされていないかつスケジュールロックされていない
+            return null
+        }
+
+    } else {
+        //インスタントロックされている
+        var calInstantLockEndTime=Calendar.getInstance()
+        calInstantLockEndTime.timeInMillis = instantLockDataList[0].startTimeInLong
+        calInstantLockEndTime.add(Calendar.MINUTE,instantLockDataList[0].durationTimeInLong.toInt())
+
+        if(nextOrDuringLockTimeList.isNotEmpty()){
+            if(commonTranslateLongToCalendar(nextOrDuringLockTimeList[0].startTimeInLong).before(Calendar.getInstance())){
+                //インスタントロックされているかつスケジュールロックされている
+                if(calInstantLockEndTime.after(commonTranslateLongToCalendar(nextOrDuringLockTimeList[0].endTimeInLong))){
+                    return calInstantLockEndTime
+                }else{
+                    return commonTranslateLongToCalendar(nextOrDuringLockTimeList[0].endTimeInLong)
+                }
+            }else{
+                //インスタントロックされているかつスケジュールロックされていない
+                return calInstantLockEndTime
+            }
+        }else{
+            //インスタントロックされているかつスケジュールロックされていない
+            return calInstantLockEndTime
         }
     }
 }
@@ -118,7 +164,7 @@ fun cancelRestartPlan(context: Context) {
     am.cancel(pending)
 }
 
-fun setRestartPlan(context: Context) {
+fun setRestartPlan(context: Context,cal:Calendar) {
 
     Log.d("■■■■■■■■■■■", "setRestartPlanが呼び出される")
     //明示的なBroadCast
@@ -135,7 +181,7 @@ fun setRestartPlan(context: Context) {
     val am: AlarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
 //    GlobalScope.launch(Dispatchers.IO) {
-        val nextLockTime = getLockTimeDao(context).getNextFromTime()
+        val nextLockTime = cal
         if (nextLockTime != null) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 Log.d("■■■■■■■■■■■", "★routeA★")
--- app/src/main/java/com/app/lockapp4/ui/HeaderInfo.kt
+++ app/src/main/java/com/app/lockapp4/ui/HeaderInfo.kt
@@ -19,106 +19,41 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.app.lockapp4.framework.database.MainViewModel
 import com.app.lockapp4.framework.utl.*
+import com.app.lockapp4.judgeNowLockedReturnUnlockTime
 import com.app.lockapp4.presentation.common.CommonVerticalGap16
 import java.util.*
 
 
 @Composable
 fun HeaderInfo(
-    viewModel:MainViewModel= hiltViewModel()
+    viewModel: MainViewModel = hiltViewModel()
 ) {
 
-    var isLockedInstantly=false
-    var endTimeLockedInstantly:Calendar?=null
+    val endTimeCal: Calendar? = judgeNowLockedReturnUnlockTime(
+        viewModel.instantLock.collectAsState(emptyList()).value,
+        viewModel.nextOrDuringLockTime.collectAsState(emptyList()).value
+    )
 
-    var isLockedScheduled=false
-    var endTimeLockedScheduled:Calendar?=null
+    if (endTimeCal == null) {
 
+        CommonVerticalGap16()
+        LockIcon(false)
+        CommonVerticalGap16()
+    } else {
 
-    val instantLockList = viewModel.instantLock.collectAsState(emptyList()).value
-
-    if(instantLockList.isEmpty()){
-
-        isLockedInstantly = false
-
-    }else{
-
-        val endTime = commonTranslateLongToCalendar(instantLockList[0].startTimeInLong)
-        endTime.add(Calendar.MINUTE,instantLockList[0].durationTimeInLong.toInt())
-        if(Calendar.getInstance().after(endTime)){
-            isLockedInstantly = false
-        }else{
-
-            isLockedInstantly = true
-            endTimeLockedInstantly = endTime
-        }
-
-    }
-
-    val nextOrDuringLockTimeList = viewModel.nextOrDuringLockTime.collectAsState(emptyList()).value
-
-    if(nextOrDuringLockTimeList.isEmpty()){
-        isLockedScheduled=false
-
-    }else{
-
-        if(commonTranslateLongToCalendar(nextOrDuringLockTimeList[0].startTimeInLong).after((Calendar.getInstance()))){
-
-            isLockedScheduled=false
-        }else{
-
-            isLockedScheduled=true
-            endTimeLockedScheduled=commonTranslateLongToCalendar(nextOrDuringLockTimeList[0].endTimeInLong)
-        }
-
-    }
-
-    CommonVerticalGap16()
-    //スケジュールロック中かどうか、インスタントロック中かどうかの2パターン×2の4パターンで場合分け
-    if (isLockedScheduled) {
-
+        CommonVerticalGap16()
         LockIcon(true)
         CommonVerticalGap16()
         Text(text = stringIsLocked, fontSize = 36.sp)
-
-        //解除時間の判定
-        if(isLockedInstantly){
-            //スケジュールロックされているかつ、インスタントロックされている場合の解除時間
-            //日時を比べて後の方の時間を表示
-            if(endTimeLockedScheduled!!.before(endTimeLockedInstantly)){
-                UnlockDateTime(endTimeLockedInstantly!!)
-            }else{
-                UnlockDateTime(endTimeLockedScheduled)
-            }
-
-        }else{
-            //スケジュールロックされているかつ、インスタントロックされていない場合の解除時間
-            UnlockDateTime(endTimeLockedScheduled!!)
-        }
+        UnlockDateTime(endTimeCal)
         EmergencyButton()
-    } else {
-
-        //スケジュールロックされていないかつ、インスタントロックされている場合の解除時間
-        if(isLockedInstantly){
-
-            LockIcon(true)
-            Text(text = stringIsLocked, fontSize = 36.sp)
-            UnlockDateTime(endTimeLockedInstantly!!)
-            EmergencyButton()
-
-            //ロックしていないパターン
-        }else{
-            LockIcon(false)
-        }
     }
-
-    CommonVerticalGap16()
 }
 
 @Composable
 fun EmergencyButton(
-    viewModel:MainViewModel= hiltViewModel()
-){
+    viewModel: MainViewModel = hiltViewModel()
+) {
 
     var showDialog by remember { mutableStateOf(false) }
     var result by remember { mutableStateOf("Result") }
@@ -134,7 +69,7 @@ fun EmergencyButton(
             contentColor = Color.White,
         )
     ) {
-        Text(color= Color.White,text = "緊急解除", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+        Text(color = Color.White, text = "緊急解除", fontSize = 20.sp, fontWeight = FontWeight.Bold)
     }
 
     if (showDialog) {
@@ -147,18 +82,18 @@ fun EmergencyButton(
                 TextButton(
                     onClick = {
                         viewModel.tapEmergencyButton(context)
-                        if(viewModel.isEmergencyUnlocked){
+                        if (viewModel.isEmergencyUnlocked) {
                             result = "OK"
                             showDialog = false
                         }
                     },
-                            colors = ButtonDefaults.textButtonColors(
-                                containerColor = Color.Red,
-                                contentColor = Color.White,
-                            )
+                    colors = ButtonDefaults.textButtonColors(
+                        containerColor = Color.Red,
+                        contentColor = Color.White,
+                    )
                 ) {
 
-                    Text(text = "残り"+(constEmergencyTapNumberRequired-viewModel.emergencyTapNumber).toString()+"回")
+                    Text(text = "残り" + (constEmergencyTapNumberRequired - viewModel.emergencyTapNumber).toString() + "回")
                 }
             },
             dismissButton = {
@@ -175,25 +110,37 @@ fun EmergencyButton(
                 Text("緊急解除")
             },
             text = {
-                    Text("ロック解除のために、"+constEmergencyTapNumberRequired.toString()+"回ボタンをタップしてください")
+                Text("ロック解除のために、" + constEmergencyTapNumberRequired.toString() + "回ボタンをタップしてください")
             },
         )
     }
 }
 
 @Composable
-fun UnlockDateTime(cal: Calendar){
+fun UnlockDateTime(cal: Calendar) {
 
     Text(text = "解除日時：${commonTranslateCalendarToStringYYYYMMDDHHMM(cal)}")
 }
 
 @Composable
-fun LockIcon(isLocked:Boolean){
+fun LockIcon(isLocked: Boolean) {
 
-    if(isLocked){
-        Icon(painter =  rememberVectorPainter(image = Icons.Default.Lock), contentDescription = null,modifier = Modifier.height(40.dp).width(40.dp))
-    }else{
-        Icon(painter =  rememberVectorPainter(image = Icons.Default.LockOpen), contentDescription = null,modifier = Modifier.height(40.dp).width(40.dp))
+    if (isLocked) {
+        Icon(
+            painter = rememberVectorPainter(image = Icons.Default.Lock),
+            contentDescription = null,
+            modifier = Modifier
+                .height(40.dp)
+                .width(40.dp)
+        )
+    } else {
+        Icon(
+            painter = rememberVectorPainter(image = Icons.Default.LockOpen),
+            contentDescription = null,
+            modifier = Modifier
+                .height(40.dp)
+                .width(40.dp)
+        )
 
     }
 }
